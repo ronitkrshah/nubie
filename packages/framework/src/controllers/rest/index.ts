a19071fd7cc1a790bacd05a36fdcdd03b02e4746
RKS <ronitkrshah@tuta.io>
2025-10-07 00:39:30 +0530

    feat: export extension abstract classes

--- packages/framework/src/controllers/rest/index.ts
+++ packages/framework/src/controllers/rest/index.ts
@@ -1,8 +1,10 @@
 import { HttpResponse } from "./utils";
 import { RateLimitRegistry, type TRateLimitConf } from "./registry";
+import { type THttpContext } from "./types";
 
 export * from "./decorators/class";
 export * from "./decorators/methods";
 export * from "./decorators/extensions";
+export * from "./abstractions";
 
-export { HttpResponse, RateLimitRegistry, TRateLimitConf };
+export { HttpResponse, RateLimitRegistry, TRateLimitConf, THttpContext };
